91613b800d3f8ebddcafeea63f1f6f03717ebfb9
Jacob Malloy <sjakem@att.net>
2019-04-29 18:32:31 -0500

    add movement

--- snake.js
+++ snake.js
@@ -3,6 +3,11 @@ var WIDTH;
 var HEIGHT;
 var speed;
 var position;
+var food;
+function moveFood()
+{
+    food = createVector(random(0,WIDTH),random(0,HEIGHT));
+}
 function setup()
 {
     createCanvas(600,600);
@@ -11,11 +16,15 @@ function setup()
     speed = createVector(1,0);
     position = createVector(WIDTH/2,HEIGHT/2);
     frameRate(10);
+    moveFood();
 }
 function draw()
 {
     scale(res);
     background(255);
+    noStroke();
+    fill(255,0,0);
+    rect(food.x,food.y,1,1);
     fill(0);
     position.add(speed);
     rect(position.x,position.y,1,1);
@@ -25,5 +34,17 @@ function keyPressed()
     if(keyCode == LEFT_ARROW)
     {
         speed = createVector(-1,0);
+    }
+    if (keyCode == RIGHT_ARROW)
+    {
+        speed = createVector(1,0);
+    }
+    if(keyCode == UP_ARROW)
+    {
+        speed = createVector(0,-1);
+    }
+    if (keyCode == DOWN_ARROW)
+    {
+        speed = createVector(0,1);
     }
 }
